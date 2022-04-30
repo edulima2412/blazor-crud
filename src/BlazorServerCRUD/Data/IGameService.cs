94818c3868f91f8745cf7abaee791680e0b744fd
Eduardo Lima <carlos.rocha@compasso.com.br>
2022-04-30 15:12:38 -0300

    initial commit

---
+++ src/BlazorServerCRUD/Data/IGameService.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorServerCRUD.Data
+{
+    public interface IGameService
+    {
+        List<Game> Games { get; set; }
+        Task LoadGamesAsync();
+        Task<Game> GetSingleGameAsync(int id);
+        Task CreateGameAsync(Game game);
+        Task UpdateGameAsync(int id, Game game);
+        Task DeleteGameAsync(int id);
+    }
+}
---
+++ src/BlazorServerCRUD/Program.cs
@@ -0,0 +1,31 @@
+using BlazorServerCRUD.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("videogamesdb"));
+
+builder.Services.AddScoped<IGameService, GameService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
